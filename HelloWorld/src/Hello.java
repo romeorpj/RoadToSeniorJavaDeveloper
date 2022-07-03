6a30a0741243f45074efd54ed6e587aceb1932f8
Richard Jackson <rpjackson2@gmail.com>
2022-07-03 13:31:09 -0500

    Beginning post grad java self study

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+   public static void main(String[] args){
+       System.out.println("Hello Richard");
+
+       int myFirstNum = (5 + 10) * (2 +2);
+       //java is strongly typed
+       // int is the datatype
+       //this is a declaration statement
+
+//       q1. print out myfirstnum
+       System.out.println(myFirstNum);
+   }
+}
