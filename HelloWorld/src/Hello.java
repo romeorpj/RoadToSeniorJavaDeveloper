f4cba7948371c3096d0feafc80fd5c4c36751263
Richard Jackson <rpjackson2@gmail.com>
2022-07-03 15:10:37 -0500

    finished java basic exercise video 1

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -9,5 +9,18 @@ public class Hello {
 
 //       q1. print out myfirstnum
        System.out.println(myFirstNum);
+
+
+/*challenge: declare mySecondNumber as an int and assign it the value 12.Declare my third number int and
+assign it the value 6.*/
+
+       int mySecondNum = 12;
+       int myThirdNum = 6;
+
+       int myTotal = mySecondNum + myThirdNum * myFirstNum;
+
+       int myLastOne = myTotal - 100;
+       System.out.println("myTotal" + " " +myTotal);
+       System.out.println("myLastOne" + " " +myLastOne);
    }
 }
